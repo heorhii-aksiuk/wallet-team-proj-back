33f76f48b96fdf97a2a47186c341320d15c0c5a3
Ivan Suprun <bender.slam@gmail.com>
2022-05-17 20:44:58 +0300

    Правка роута статистики

--- router/statistics-routes.js
+++ router/statistics-routes.js
@@ -1,8 +1,15 @@
 const { Router } = require('express');
+const {
+    validateStatisticsQuery,
+} = require('../validaton/statistics-validation');
 const Controllers = require('../controllers/statistics-controllers');
 
 const statisticsRoutes = Router();
 
-statisticsRoutes.get('/statistics', Controllers.getStatistics);
+statisticsRoutes.get(
+    '/statistics',
+    validateStatisticsQuery,
+    Controllers.getStatistics,
+);
 
 module.exports = statisticsRoutes;
